994a92be1cf173d073d1fad5f50f1d2b2fc054f1
kartik gupta <gupta.kartik18kg@gmail.com>
2024-04-05 07:47:58 +0530

    one

--- client/src/routes/apiEndpoints.ts
+++ client/src/routes/apiEndpoints.ts
@@ -3,5 +3,8 @@ export default {
     ALL: {
       URL: `${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs`,
     },
+    ONE: {
+      URL: `${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs`,
+    },
   },
 };
